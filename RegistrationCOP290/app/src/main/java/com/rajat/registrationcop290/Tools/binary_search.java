db057cc14bb464b54e00733752372d252cf5eb85
arpitjain1995 <arpitjain1995@gmail.com>
2016-01-23 00:37:49 +0530

    binary search and mergesort fixed

---
+++ RegistrationCOP290/app/src/main/java/com/rajat/registrationcop290/Tools/binary_search.java
@@ -0,0 +1,35 @@
+package com.rajat.registrationcop290.Tools;
+
+
+import java.util.LinkedList;
+
+/**
+ * Created by Lenovo on 1/20/2016.
+ */
+public class binary_search {
+    public LinkedList<String> search (String key,String[] array){
+        int j = search_index(key,array,0,array.length);
+        LinkedList<String> list=new LinkedList<String>();
+        list.add(array[j]);
+        for(int i=j-1;i>=0;i--){
+            if(equal(key,array[i])) list.addFirst(array[i]);
+            else break;
+        }
+        for(int i=j+1;i<array.length;i++){
+            if(equal(key,array[i])) list.addLast(array[i]);
+            else break;
+        }
+        return list;
+    }
+    private int search_index(String key,String[] array,int inital,int last){
+        int mid =(last+inital)/2;
+        if(inital>last)throw new NullPointerException();
+        if(equal(key,array[mid]))return mid;
+        else if((key.compareToIgnoreCase(array[mid])>0))return search_index(key,array,mid+1,last);
+        else return search_index(key,array,inital,mid-1);
+    }
+    private boolean equal(String a, String b ){
+    	if (b.length()>=a.length())return a.contentEquals(b.substring(0,a.length()));
+    	else return false;
+    	}
+}
--- RegistrationCOP290/app/src/main/java/com/rajat/registrationcop290/Tools/mergesort.java
+++ RegistrationCOP290/app/src/main/java/com/rajat/registrationcop290/Tools/mergesort.java
@@ -1,13 +1,15 @@
-package com.rajat.registrationcop290.Volley;
+package com.rajat.registrationcop290.Tools;
+
+
 
 /**
  * Created by Lenovo on 1/19/2016.
  */
 
     public class mergesort {
-        public mergesort(String[] tobe,int i,int j){
-            String[] aux=new String[(j-i)+1];
-            sort(tobe,aux,i,j);
+        public mergesort(String[] tobe){
+            String[] aux=new String[tobe.length];
+            sort(tobe,aux,0,tobe.length-1);
         }
         private void merge(String[] tobe,String[] aux,int start,int mid,int end){
             for(int k=start;k<=end;k++){
@@ -17,7 +19,7 @@ package com.rajat.registrationcop290.Volley;
             for(int k=start;k<=end;k++){
                 if(start>mid){tobe[k]=aux[j++];}
                 else if(j>end){tobe[k]=aux[start++];}
-                else if(aux[start].compareToIgnoreCase(aux[j])>=0){tobe[k]=aux[start++];}
+                else if(aux[start].compareToIgnoreCase(aux[j])<=0){tobe[k]=aux[start++];}
                 else{tobe[k]=aux[j++];}
             }
         }
