f4a50de42e0bdddc819e34c748d9fd25eb20c7c1
rajatsurana <rajat.surana123@gmail.com>
2016-01-24 00:18:46 +0530

    updated

---
+++ RegistrationCOP290/app/src/main/java/com/rajat/registrationcop290/RegisterActivity.java
@@ -0,0 +1,246 @@
+package com.rajat.registrationcop290;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.InputType;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.rajat.registrationcop290.Tools.CheckNetwork;
+import com.rajat.registrationcop290.Tools.CustomAutoCompleteView;
+import com.rajat.registrationcop290.Tools.CustomTextWatcher;
+import com.rajat.registrationcop290.Tools.Student;
+import com.rajat.registrationcop290.Tools.Tools;
+import com.rajat.registrationcop290.Tools.Validate;
+import com.rajat.registrationcop290.Volley.CallVolley;
+import com.rajat.registrationcop290.Volley.VolleySingleton;
+
+import java.io.UnsupportedEncodingException;
+
+
+public class RegisterActivity extends AppCompatActivity {
+    MediaPlayer sound = null;
+    Toolbar toolbar;
+    int titleId;
+    CustomAutoCompleteView cacv;
+    public ArrayAdapter<String> enAdap;
+    CustomAutoCompleteView cacvName;
+    public ArrayAdapter<String> nameAdap;
+    public String[] enNums = new String[] {"Please search...","Don't search...","Please search...","Don't search...","Please search...","Don't search..."};
+    public String[] stdNames = new String[] {"Please search...","Don't search...","Please search...","Don't search...","Please search...","Don't search..."};
+    EditText teamName,
+            entryNum1, entryNum2, entryNum3,
+            name1, name2, name3;
+
+    String TeamName="",
+            entryNumber1="", entryNumber2="", entryNumber3="",
+            studentName1="", studentName2="", studentName3="";
+    Button submit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        toolbar=(Toolbar)findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+        setTitle((Html.fromHtml("<font color=\"#FFFFFF\">" + "Registration COP290" + "</font>")));
+        Student.getS();
+        //ugetS();
+        Student.getEntryNum();
+        Student.getNames();
+        enNums=Student.studentEntryNum;
+        stdNames=Student.studentNames;
+        initializeViews();
+    }
+    public void initializeViews(){
+        teamName = (EditText)findViewById(R.id.team_name);
+        entryNum1 = (EditText)findViewById(R.id.EntryNum1);
+        entryNum2 = (EditText)findViewById(R.id.EntryNum2);
+        entryNum3 = (EditText)findViewById(R.id.EntryNum3);
+        name1 = (EditText)findViewById(R.id.Name1);
+        name2 = (EditText)findViewById(R.id.Name2);
+        name3 = (EditText)findViewById(R.id.Name3);
+        cacv = new CustomAutoCompleteView(RegisterActivity.this);
+        cacvName= new CustomAutoCompleteView(RegisterActivity.this);
+
+        enAdap = new ArrayAdapter<String>(RegisterActivity.this, android.R.layout.simple_dropdown_item_1line, enNums);
+        nameAdap = new ArrayAdapter<String>(RegisterActivity.this, android.R.layout.simple_dropdown_item_1line, stdNames);
+        cacv.setThreshold(1);
+        cacv.setAdapter(enAdap);
+        enAdap.notifyDataSetChanged();
+        cacvName.setThreshold(1);
+        cacvName.setAdapter(nameAdap);
+        nameAdap.notifyDataSetChanged();
+
+        teamName.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        entryNum1.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        entryNum2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        entryNum3.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        //name1.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        //name2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        //name3.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        teamName.addTextChangedListener(new CustomTextWatcher(teamName,0));
+        entryNum1.addTextChangedListener(new CustomTextWatcher(entryNum1,1,cacv,enAdap));
+
+        entryNum2.addTextChangedListener(new CustomTextWatcher(entryNum2,1,cacv,enAdap));
+        entryNum3.addTextChangedListener(new CustomTextWatcher(entryNum3,1,cacv,enAdap));
+        name1.addTextChangedListener(new CustomTextWatcher(name1,2,cacvName,nameAdap));
+        name2.addTextChangedListener(new CustomTextWatcher(name2,2,cacvName,nameAdap));
+        name3.addTextChangedListener(new CustomTextWatcher(name3,2,cacvName,nameAdap));
+        submit =(Button)findViewById(R.id.submitNames);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSubmitClick();
+            }
+        });
+    }
+    public void onSubmitClick(){
+        Validate validate1=new Validate();
+        //boolean invalid=false;
+        TeamName =teamName.getText().toString();
+        entryNumber1=entryNum1.getText().toString().toUpperCase();
+        entryNumber2=entryNum2.getText().toString().toUpperCase();
+        entryNumber3=entryNum3.getText().toString().toUpperCase();
+        studentName1=name1.getText().toString().toUpperCase();
+        studentName2=name2.getText().toString().toUpperCase();
+        studentName3=name3.getText().toString().toUpperCase();
+        boolean valid=false;
+        if(   teamName.getText().length()>0
+                && validate1.validate_entryno(entryNumber1)&& validate1.validate_name(studentName1)
+                && validate1.validate_entryno(entryNumber2)&& validate1.validate_name(studentName2)
+                && !entryNumber1.equals(entryNumber2) && !studentName2.equals(studentName1)
+                ) {
+
+            if(entryNum3.getText().length()>0 || name3.getText().length()>0){
+                if(validate1.validate_entryno(entryNumber3)&& validate1.validate_name(studentName3)
+                        && !entryNumber1.equals(entryNumber3) && !entryNumber3.equals(entryNumber2)
+                        && !studentName2.equals(studentName3) && !studentName3.equals(studentName1)
+                        ){
+                    valid=true;
+                }else{
+                    if (sound == null) {
+                        if (!validate1.validate_entryno(entryNumber3)) {
+                            sound = MediaPlayer.create(this, R.raw.sentry3);
+                        } else if (!validate1.validate_name(studentName3)) {
+                            sound = MediaPlayer.create(this, R.raw.names3);
+                        }else if(entryNumber1.equals(entryNumber3)){
+                            sound = MediaPlayer.create(this, R.raw.similar_entry);////////////////
+                        }else if(entryNumber2.equals(entryNumber3)){
+                            sound = MediaPlayer.create(this, R.raw.similar_entry);////////////////
+                        }else if(studentName3.equals(studentName1)){
+                            sound = MediaPlayer.create(this, R.raw.similar_names);////////////////
+                        }else if(studentName2.equals(studentName3)){
+                            sound = MediaPlayer.create(this, R.raw.similar_names);////////////////
+                        }
+                        sound.start();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                sound.release();
+                                sound = null;
+                            }
+                        }, sound.getDuration());
+                    }else {
+                        Toast.makeText(RegisterActivity.this, "Have some patience duh...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }else{
+                valid=true;
+            }
+            //Toast.makeText(RegisterActivity.this,"Submit Clicked",Toast.LENGTH_SHORT).show();
+
+        }else{
+            if (sound == null) {
+                if(teamName.getText().length()==0){
+                    sound = MediaPlayer.create(this, R.raw.team);
+                }else if(!validate1.validate_entryno(entryNumber1)){
+                    sound = MediaPlayer.create(this, R.raw.sentry1);
+                }else  if(!validate1.validate_name(studentName1) ){
+                    sound = MediaPlayer.create(this, R.raw.names1);
+                }else if(!validate1.validate_entryno(entryNumber2)){
+                    sound = MediaPlayer.create(this, R.raw.sentry2);
+                }else  if(!validate1.validate_name(studentName2) ){
+                    sound = MediaPlayer.create(this, R.raw.names2);
+                }else if(entryNumber1.equals(entryNumber2)){
+                    sound = MediaPlayer.create(this, R.raw.similar_entry);////////////////
+                }else if(studentName2.equals(studentName1)){
+                    sound = MediaPlayer.create(this, R.raw.similar_names);
+                }
+                sound.start();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        sound.release();
+                        sound = null;
+                    }
+                }, sound.getDuration());
+            }else {
+                Toast.makeText(RegisterActivity.this, "Have some patience duh...", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if(valid){
+            CheckNetwork chkNet = new CheckNetwork(RegisterActivity.this);
+            String URL = "http://agni.iitd.ernet.in/cop290/assign0/register/";
+            if (chkNet.checkNetwork()) {
+                VolleySingleton.getInstance(RegisterActivity.this).getRequestQueue().getCache().clear();
+
+                //CallVolley.makeRegistrationCall(URL, TeamName, entryNumber1, studentName1,
+                //      entryNumber2, studentName2,
+                //    entryNumber3, studentName3, RegisterActivity.this);
+            } else {
+                Tools.showAlertDialog("Internet Unavailable", RegisterActivity.this);
+            }
+        }else{
+            final Animation animat= AnimationUtils.loadAnimation(this, R.anim.shake);
+            submit.startAnimation(animat);
+            //int i=0;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    submit.startAnimation(animat);
+                }
+            },100);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    submit.startAnimation(animat);
+                }
+            }, 200);
+
+
+
+
+        }
+    }
+    boolean doubleBackToExitPressedOnce=false;
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+
+}
---
+++ RegistrationCOP290/app/src/main/java/com/rajat/registrationcop290/Volley/CallVolley.java
@@ -0,0 +1,138 @@
+package com.rajat.registrationcop290.Volley;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.util.Log;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.rajat.registrationcop290.R;
+import com.rajat.registrationcop290.Tools.Tools;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+public class CallVolley {
+        private static ProgressDialog pDialog;
+               private static MediaPlayer dsound=null;
+        private static void setCustomRetryPolicy(StringRequest jsonObjReq) {
+                Log.i("rajat", "setCustomRetryPolicy");
+                jsonObjReq.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS*2, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        }
+
+        public static void makeRegistrationCall(String url, final String teamname,final String entrynum1,final String name1,final String entrynum2,final String name2,final String entrynum3,final String name3, final Context context)
+        {
+                pDialog=  Tools.showProgressBar(context);
+
+                //RequestQueue queue = VolleySingleton.getInstance(context).getRequestQueue();
+
+                        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>()
+                        {
+
+
+                                @Override
+                                public void onResponse(String response)
+                                {
+                                        try
+                                        {
+                                                Log.i("rajat", " onResponseActive " + response);
+                                                SubmitApiJsonParser(response, context);
+                                                pDialog.dismiss();
+                                        }
+                                        catch (Exception localException)
+                                        {
+                                                Log.i("rajat"," onResponseException "+localException.getMessage());
+                                                localException.printStackTrace();
+                                        }
+                                }
+                        }
+                                , new Response.ErrorListener()
+                        {
+                                @Override
+                                public void onErrorResponse(VolleyError error)
+                                {
+                                        Log.i("rajat", "onErrorResponse" + error.toString());
+                                           pDialog.dismiss();
+
+
+                                }
+                        })
+                        {
+                                @Override
+                                protected Map<String, String> getParams()
+                                {
+                                        Log.i("rajat", " onResponseParamsExecute");
+                                        Map<String, String> params = new HashMap<String, String>();
+
+                                        try {
+                                                Log.i("rajat", " onResponseParams" + teamname);
+                                                params.put("teamname", teamname);
+                                                params.put("entry1",entrynum1);
+                                                params.put("name1",name1);
+                                                params.put("entry2",entrynum2);
+                                                params.put("name2",name2);
+                                                params.put("entry3",entrynum3);
+                                                params.put("name3",name3);
+                                                Log.i("rajat","request: "+params);
+                                        }
+                                        catch (Exception e)
+                                        {
+
+                                                Log.i("rajat", "onExceptionParams" + e.toString());
+                                        }
+
+                                        return params;
+                                }
+                        };
+
+                        setCustomRetryPolicy(request);
+                        VolleySingleton.getInstance(context).addToRequestQueue(request);
+                }
+                public static void SubmitApiJsonParser(String JsonStringResult,Context con)
+                {
+                        try {
+                                String status = "";
+                                String message="";
+
+
+                                JSONObject resultJson = new JSONObject(JsonStringResult);
+                                if (resultJson.has("RESPONSE_SUCCESS"))
+                                {
+                                        status = resultJson.getString("RESPONSE_SUCCESS");
+                                        message = resultJson.getString("RESPONSE_MESSAGE");
+                                        if(status.equals("0"))
+                                        {
+                                                if(message.equals("Data not posted!"))
+                                                {
+                                                        Tools.showAlertDialog("Data not posted!",con);
+                                                        dsound = MediaPlayer.create(con,R.raw.data_not_posted);
+                                                        dsound.start();
+                                                }
+                                                else if(message.equals("User already registered"))
+                                                {
+                                                        Tools.showAlertDialog("User Already Registered",con);
+                                                        dsound = MediaPlayer.create(con,R.raw.user_already_registered);
+                                                        dsound.start();
+                                                }
+                                        }
+                                        else
+                                        {
+                                                if(message.equals("Registration completed"))
+                                                {
+                                                        Tools.showAlertDialog("Registration completed",con);
+                                                        dsound = MediaPlayer.create(con, R.raw.registration_completed);
+                                                        dsound.start();
+
+                                                }
+                                        }
+                                }
+                        }
+                        catch (Exception e)
+                        {
+                                Log.i("rajat", e.getLocalizedMessage());
+                        }
+                }
+        }
